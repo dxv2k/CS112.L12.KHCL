ce7064b92062893cb5cb65c8046de31de80c1e19
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-09-16 13:35:27 +0700

    Update input, output and temporary variables

--- Week1/BOT/solution.py
+++ Week1/BOT/solution.py
@@ -1,13 +1,13 @@
 n = int(input())
-a = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 p = 0
 q = 0
 temp_p = 0
-temp_profit = a[0]
+temp_profit = arr[0]
 profit = -99999
 
 for i in range(1, n):
-    temp_profit = temp_profit + a[i]
+    temp_profit = temp_profit + arr[i]
     if temp_profit > profit:
         profit = temp_profit
         q = i
