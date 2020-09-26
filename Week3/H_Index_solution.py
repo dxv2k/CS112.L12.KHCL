4000e0a2d81e3f764bbb3fee2825c02de32bab65
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-09-26 22:32:39 +0700

    Create main function

--- Week3/H_Index_solution.py
+++ Week3/H_Index_solution.py
@@ -1,10 +1,17 @@
-n = int(input())
-arr = list(map(int, input().split(' ')))
-arr.sort(reverse = True)
-h_index = 0
-for i, num_paper in enumerate(arr):
-    if num_paper >= i+1:
-        h_index = i+1
-    else:
-        break
-print(h_index)
+def H_Index_Calc(n, arr):
+    arr.sort(reverse = True)
+    h_index = 0
+    for i, num_paper in enumerate(arr):
+        if num_paper >= i+1:
+            h_index = i+1
+        else:
+            break
+    return h_index
+
+def main():
+    n = int(input())
+    arr = list(map(int, input().split(' ')))
+    print(H_Index_Calc(n, arr))
+
+if __name__ == "__main__":
+    main()
