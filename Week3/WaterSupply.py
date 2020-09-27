728c0bfcd8cb599fae55f8a1533e17669b6cba36
knightV <9569076+dxv2k@users.noreply.github.com>
2020-09-27 12:48:01 +0700

    Add input, main function, working on solution

---
+++ Week3/WaterSupply.py
@@ -0,0 +1,19 @@
+MODULE = 10**9 + 7 
+
+def solution(n,m,arr): 
+
+
+def main(): 
+	n,m = map(int,input().split())
+	arr = []
+	for i in range(m): 
+		a,b = map(int,input().split())
+		arr.append([a,b])
+#	print(n,m) 
+#	print(arr)
+
+
+if __name__ == "__main__": 
+	main() 
+
+
