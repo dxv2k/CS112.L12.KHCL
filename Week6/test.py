c3cfe8a955c3b5a0682b051dd76e9716d7f289bb
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-10-29 19:54:37 +0700

    no idea what I'm doing

--- Week6/test.py
+++ Week6/test.py
@@ -1,21 +1,45 @@
-# mask = (1 << 12) - 1
+mask = (1 << 12) - 1
 
-# def number_bit_zero(x,mask):
-#     cnt = 0
-#     for pos in range(12):
-#         if (x >> pos) & 1 == 0 and (mask >> pos) & 1 == 1:
-#             cnt += 1
-#     return cnt
+# count number of 1 bits and see how much left with
+# 12 bit in the mask  
+def number_bit_zero(x,mask):
+    cnt = 0
+    for pos in range(12):
+        if (x >> pos) & 1 == 0 and (mask >> pos) & 1 == 1:
+            cnt += 1
+    return cnt
+
+print(number_bit_zero(mask&4,mask))
+
+# n, k = map(int,input().split())
+# a = list(map(int,input().split()))
 
 
-# x = int(input())
-# print(number_bit_zero(x,mask))
+# cnt = 0
+# for pos in range(12):
+#     if (mask >> pos) & 1 == 1:
+#         tmp = -1
+#         for x in a:
+#             if (x >> pos) & 1 == 0 :
+#                 #print(x)
+#                 #print('------------------------------')
+#                 tmp_zero = number_bit_zero(mask & x,mask)
+#         #print(tmp_zero)
+#                 if tmp_zero > tmp:
+#                     tmp = tmp_zero
+#                     arg = x
+#         if (tmp == -1):
+#             print('NO')
+#             quit()
 
-n,k = map(int, input().split())
-arr = tuple(map(bin, map(int, input().split() )))
+#         cnt += 1
+#         mask &= arg
+#     #print(mask)
+#         if (mask == 0): break
 
-count = [0] * 12 
-
-print(arr)
+# if cnt <= k:
+#     print('YES')
+# else:
+#     print('NO')
 
 
