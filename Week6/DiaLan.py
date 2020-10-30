61266ad660e4653d4c56858c8c56e866eb55f4ca
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-10-30 15:37:19 +0700

    clean up solution file

--- Week6/DiaLan.py
+++ Week6/DiaLan.py
@@ -1,7 +1,7 @@
 MAX_LENGTH = 12
-mask = (1 << 12) - 1 # bin(4095) 
 
 def count_zero_bit(x,mask):
+    # count zero bit by compare x with mask
     cnt = 0
     for pos in range(MAX_LENGTH):
         if (x >> pos) & 1 == 0 and (mask >> pos) & 1 == 1:
@@ -9,6 +9,7 @@ def count_zero_bit(x,mask):
     return cnt
 
 def solution(n,k,a): 
+    mask = (1 << 12) - 1 # bin(4095) 
     cnt = 0 
     for pos in range(MAX_LENGTH):
         if (mask >> pos) & 1 == 1:
@@ -16,11 +17,11 @@ def solution(n,k,a):
             for x in a:
                 if (x >> pos) & 1 == 0 :
                     print(x)
-                    print('------------------------------')
                     tmp_zero = count_zero_bit(mask & x,mask)
                     if tmp_zero > tmp:
                         tmp = tmp_zero
                         arg = x
+
             if (tmp == -1):
                 print('NO')
                 quit()
@@ -28,9 +29,7 @@ def solution(n,k,a):
             cnt += 1
             mask &= arg
             if (mask == 0): break
-
         print(mask)
-
     if cnt <= k: 
         print("YES")
     else: 
